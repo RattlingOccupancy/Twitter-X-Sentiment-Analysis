55716bc1ff25343085440738dc77a4c4daa8b3e5
Hardik Nautiyal <23BCS11339@cuchd.in>
2026-03-25 12:56:36 +0530

    implemented preprocessing pipeline

--- preprocessing/test_pipeline.py
+++ preprocessing/test_pipeline.py
@@ -14,9 +14,12 @@ from preprocessing.pipeline import PreprocessingPipeline
 if __name__ == "__main__":
     pipeline = PreprocessingPipeline()
 
-    sample = "OMG!!! I can't believe this 😭😭 #heartbroken @user https://t.co/xyz"
+    sample = "OMG!!! I can't believe this.          this is miracle😭😭 #heartbroken @user https://t.co/xyz"
 
     output = pipeline.process(sample)
 
     print("Cleaned Text:", output["cleaned_text"])
     print("Input IDs shape:", output["input_ids"].shape)
+
+
+    
